59c36efb46bcf729fa7a9f90f9ea8345f55f9f98
ELiza Maka <elizamaka95@gmail.com>
2024-06-26 15:08:56 +0545

    get recent blogs and all blogs of a user

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import BlogViewSet
+from .views import BlogViewSet, RecentBlogsViewSet, AllBlogsViewSet
 
 router = DefaultRouter()
-router.register(r'', BlogViewSet, basename='blog')
+router.register(r'blog', BlogViewSet, basename='blog')
+router.register(r'recent', RecentBlogsViewSet, basename='recent')
+router.register(r'all', AllBlogsViewSet, basename='all')
 
 urlpatterns = [
     path('', include(router.urls)),
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
 
 from .models import Blog
 from .serializers import BlogSerializer
+
+from users.models import User
 
 # Create your views here.
 class BlogViewSet(viewsets.ModelViewSet):
@@ -16,3 +19,36 @@ class BlogViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Blog.objects.filter(user=self.request.user)
+
+
+class RecentBlogsViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id')
+        if user_id is None:
+            raise NotFound(detail="user_id parameter is required.")
+        
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            raise NotFound(detail=f"User with id {user_id} does not exist.")
+        
+        return Blog.objects.filter(user=user).order_by('-created_at')[:5]
+
+class AllBlogsViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id')
+        if user_id is None:
+            raise NotFound(detail="user_id parameter is required.")
+        
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            raise NotFound(detail=f"User with id {user_id} does not exist.")
+        
+        return Blog.objects.filter(user=user)
